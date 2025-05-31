b4e6ec5cd9eed27d8a1071e2fce405df34e7da9a
Zhang-XC <zhang_xiaocheng@outlook.com>
2025-05-31 20:33:58 +0200

    Add entries to settings

--- common/settings.py
+++ common/settings.py
@@ -5,8 +5,14 @@ import sys
 DIR_BASE = os.path.dirname(os.path.dirname(__file__))
 sys.path.append(DIR_BASE)
 
+DIR_REPORT = os.path.join(DIR_BASE, "report", "temp")
+
+URL_HOST = "https://127.0.0.1:8888"
+
 FILE_PATH = {
-    "DATABASE": os.path.join(DIR_BASE, "database/ecommerce.db"),
-    "SCHEMA": os.path.join(DIR_BASE, "database/schema.sql"),
-    "EXTRACT": os.path.join(DIR_BASE, "test_framework/data/extracted.yaml"),
+    "DATABASE": os.path.join(DIR_BASE, "database", "ecommerce.db"),
+    "SCHEMA": os.path.join(DIR_BASE, "database", "schema.sql"),
+    "EXTRACT": os.path.join(DIR_BASE, "test_framework", "data", "extracted.yaml"),
+    "JUNIT_XML": os.path.join(DIR_BASE, "report", "results.xml"),
+    "ENV_XML": os.path.join(DIR_BASE, "environment.xml"),
 }
