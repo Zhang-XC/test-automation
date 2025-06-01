abb95beaa2673c50229a0b8e862be10eff9b6742
Zhang-XC <zhang_xiaocheng@outlook.com>
2025-06-01 23:33:43 +0200

    Split testcases into multiple yaml files

--- test_framework/tests/test_app.py
+++ test_framework/tests/test_app.py
@@ -9,7 +9,13 @@ from test_framework.core.run_testcase import run_testcase
 @allure.feature(next(m_id) + "User Management")
 class TestUserManager:
     @pytest.mark.run(order=1)
-    @pytest.mark.parametrize("testcase", load_testcase("testcases.yaml"))
-    def test_add_user(self, testcase):
+    @pytest.mark.parametrize("testcase", load_testcase("test_register.yaml"))
+    def test_register(self, testcase):
+        allure.dynamic.title(testcase['case_name'])
+        run_testcase(testcase)
+
+    @pytest.mark.run(order=2)
+    @pytest.mark.parametrize("testcase", load_testcase("test_login.yaml"))
+    def test_login(self, testcase):
         allure.dynamic.title(testcase['case_name'])
         run_testcase(testcase)
