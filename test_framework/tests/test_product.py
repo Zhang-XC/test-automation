d49378cb35e4a26b8ea08b55fa6a7840fddfb0ab
Zhang-XC <zhang_xiaocheng@outlook.com>
2025-06-29 22:32:05 +0200

    Add testcases for /products endpoint

---
+++ test_framework/tests/test_product.py
@@ -0,0 +1,19 @@
+import allure
+import pytest
+
+from test_framework.core.read_yaml import load_testcase
+from test_framework.core.generate_id import m_id, c_id, generate_order
+from test_framework.core.run_testcase import run_testcase
+
+
+order = generate_order()
+
+
+@allure.feature(next(m_id) + "Product Management")
+class TestProductManager:
+    @allure.story(next(c_id) + "View products")
+    @pytest.mark.run(order=next(order))
+    @pytest.mark.parametrize("testcase", load_testcase("test_products.yaml"))
+    def test_login(self, testcase):
+        allure.dynamic.title(testcase['case_name'])
+        run_testcase(testcase)
